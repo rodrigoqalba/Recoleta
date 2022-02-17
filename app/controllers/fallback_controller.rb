804976d6fed7a5ee64b1cf430c2799c0bf3f76b7
Rodrigo Alba <rodrigo@megabadvisors.com>
2022-02-17 19:18:10 -0400

    deploying application

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -1,6 +1,6 @@
 # Controller logic: fallback requests for React Router.
 # Leave this here to help deploy your app later!
-class FallbackController < ActionController::Base
+class FallbackController < ActionController::API
 
     def index
       # React app index page
